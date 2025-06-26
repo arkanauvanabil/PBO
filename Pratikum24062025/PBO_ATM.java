1c2a4089e9f76ae0d66d90e8d25dcfb072d655f8
arkanauvanabil <arkanauva.nabil@gmail.com>
2025-06-27 06:16:35 +0700

    Add files via upload

---
+++ Pratikum24062025/PBO_ATM.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+/**
+ *
+ * @author ARKAN_AUVA
+ */
+public class PBO_ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
